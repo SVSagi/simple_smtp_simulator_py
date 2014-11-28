f2311269319d4733102af271f5602d0ba26a9382
SVSagi <sumanvarma18@gmail.com>
2014-11-28 17:30:17 +0530

    b1 working test1

--- smtp_simulator_1/simulator/smtp_simulator_gui.py
+++ smtp_simulator_1/simulator/smtp_simulator_gui.py
@@ -1,7 +1,7 @@
 import ss_tray
 import wx.html
 import smtp_simulator_server as smtp_ss
-import server_test1 as test, time
+import os, email, datetime, time, threading
 
 app_name = "SMTP Simulator"
 app_version = "0.1b1"
@@ -16,7 +16,7 @@ class EmailTablePanel(wx.Panel):
         self.eml_table.InsertColumn(2, 'To', width=120)
         self.eml_table.InsertColumn(3, 'Subject', width=180)
         self.eml_table.InsertColumn(4, 'Attachment', width=50)
-        self.eml_table.InsertColumn(5, 'Email ID', width=50)
+        self.eml_table.InsertColumn(5, 'Email', width=50)
         
         self.et_bs1 = wx.BoxSizer(wx.VERTICAL)
         self.et_bs1.Add(self.eml_table, 1, wx.EXPAND|wx.ALL)
@@ -26,32 +26,66 @@ class EmailTablePanel(wx.Panel):
         self.il = wx.ImageList(16, 16)
         img_idx = self.il.Add(wx.Image("icons/attach.png", wx.BITMAP_TYPE_PNG).ConvertToBitmap())
         self.eml_table.SetImageList(self.il, wx.IMAGE_LIST_SMALL)
-        '''
+        
         self.eml_table.InsertStringItem(0, "23:42 26-11-2014")
         self.eml_table.SetStringItem(0, 1, "maxadmin@maximo.com")
         self.eml_table.SetStringItem(0, 2, "user1@maximo.com")
         self.eml_table.SetStringItem(0, 3, "Your Maximo passwors was changed")
         self.eml_table.SetStringItem(0, 4, "PDF")
-        
-        self.eml_table.InsertStringItem(1, "23:20 26-11-2014")
-        self.eml_table.SetStringItem(1, 1, "maxadmin@maximo.com")
-        self.eml_table.SetStringItem(1, 2, "2@maximo.com")
-        self.eml_table.SetStringItem(1, 3, "Your Maximo passwors was changed")
-        self.eml_table.SetStringItem(1, 4, "PDF")
+
         #self.eml_table.SetItemImage(0, 1, img_idx)
-        #self.eml_table.InsertImageStringItem(1, "", img_idx) 
+        #self.eml_table.InsertImageStringItem(1, "", img_idx)
+        #self.eml_table.Append(("15:20 28-11-2014", "user3@client.com"))
+        '''
         
         self.eml_table.Bind(wx.EVT_LEFT_DOWN, self.OnListLeftDown)
         self.eml_table.Bind(wx.EVT_RIGHT_DOWN, self.OnListRightDown)
         
+        self.loadEmailFiles(None)
+     
+    def deleteEmailFile(self, event):
+        index = self.eml_table.GetFirstSelected()
+        item = self.eml_table.GetItem(itemId=index, col=5)
+        os.remove('emails/'+item.GetText())
+        self.eml_table.DeleteItem(index)
+        
     def OnListLeftDown(self, event):
         x,y = event.GetPosition()
         row,flags = self.eml_table.HitTest( (x,y) )
-        self.Parent.Parent.ED_panel.html_win.SetPage('<html><body><i>Email: '+str(row)+'</i></body></html>')
+        #self.Parent.Parent.ED_panel.html_win.SetPage('<html><body><i>Email: '+str(row)+'</i></body></html>')
+        #count = self.eml_table.GetItemCount()
+        #cols = self.eml_table.GetColumnCount()
+        
+        item = self.eml_table.GetItem(itemId=row, col=5)
+        #print item.GetText()
+        f1 = open('emails/'+item.GetText()) 
+        emsg = email.message_from_string(str(f1.read()))
+        f1.close()
+        self.Parent.Parent.ED_panel.html_win.SetPage('<html>'+self.getEmailBody(emsg)+'</html>')
         self.eml_table.Select(row)
             
     def OnListRightDown(self, event):
         pass
+    
+    def loadEmailFiles(self, event):
+        self.eml_table.DeleteAllItems()
+        for file in os.listdir("emails"):
+            if file.endswith(".eml"):
+                f1 = open('emails/'+file) 
+                emsg = email.message_from_string(str(f1.read()))
+                f1.close()
+                self.eml_table.Append((emsg['date'], emsg['from'], emsg['to'], emsg['subject'], '', file))#os.path.splitext(file)[0]
+        print 'Emails Loaded'
+                
+    def getEmailBody(self, emsg):
+        body = ""
+        if emsg.is_multipart():
+            for payload in emsg.get_payload():
+                body+= payload.get_payload()
+        else:
+            body =  emsg.get_payload()
+                
+        return body        
 
 class EmailDetailsPanel(wx.Panel):
     def __init__(self, parent):
@@ -82,14 +116,17 @@ class MainPanel(wx.Panel):
 class MainFrame(wx.Frame):
     
     def __init__(self):
-        wx.Frame.__init__(self, None, title=app_name+" v"+app_version, size=(600, 500), pos=(400, 100))
+        wx.Frame.__init__(self, None, title=app_name+" v"+app_version, size=(700, 500), pos=(100, 100))
         self.main_panel = MainPanel(self)
         self.status_bar = self.CreateStatusBar()
         self.status_bar.SetFieldsCount(2)
         self.status_bar.SetStatusWidths([80,-1])
         self.tbIcon = ss_tray.TrayIcon(self)
+        self.last_eml = ""
         
         self.smtp_mgnr = None
+        self.mon_thread = None
+        self.stop_mon = False
  
         self.Bind(wx.EVT_ICONIZE, self.onMinimize)
         self.Bind(wx.EVT_CLOSE, self.onClose)
@@ -97,10 +134,18 @@ class MainFrame(wx.Frame):
         toolbar = self.CreateToolBar(wx.TB_NODIVIDER | wx.TB_FLAT | wx.TB_DOCKABLE)
         startTool = toolbar.AddLabelTool(wx.ID_ANY, 'Start', wx.Bitmap('icons/computer.png'), shortHelp='Start Simulator')
         stopTool  = toolbar.AddLabelTool(wx.ID_ANY, 'Stop', wx.Bitmap('icons/computer_delete.png'), shortHelp='Stop Simulator')
+        refresh_emls  = toolbar.AddLabelTool(wx.ID_ANY, 'Refresh', wx.Bitmap('icons/arrow_refresh.png'), shortHelp='Refresh Emails')
+        monitor_emails  = toolbar.AddLabelTool(wx.ID_ANY, 'Monitor', wx.Bitmap('icons/flag_blue.png'), shortHelp='Monitor Emails')
+        stop_monitor_emails  = toolbar.AddLabelTool(wx.ID_ANY, 'Stop Monitor', wx.Bitmap('icons/flag_red.png'), shortHelp='Stop Monitoring Emails')
+        delete_sel_eml  = toolbar.AddLabelTool(wx.ID_ANY, 'Delete Email', wx.Bitmap('icons/delete.png'), shortHelp='Delete Selected Email')
         toolbar.Realize()
 
         self.Bind(wx.EVT_TOOL, self.startSMTPSS, startTool)
         self.Bind(wx.EVT_TOOL, self.stopSMTPSS, stopTool)
+        self.Bind(wx.EVT_TOOL, self.main_panel.ET_panel.loadEmailFiles, refresh_emls)
+        self.Bind(wx.EVT_TOOL, self.doMonitor, monitor_emails)
+        self.Bind(wx.EVT_TOOL, self.stopMonitor, stop_monitor_emails)
+        self.Bind(wx.EVT_TOOL, self.main_panel.ET_panel.deleteEmailFile, delete_sel_eml)
         
         menuBar = wx.MenuBar()
 
@@ -117,13 +162,53 @@ class MainFrame(wx.Frame):
         menuBar.Append(aboutMenu,  "&Help")
         self.SetMenuBar(menuBar)
         
+        self.Bind(wx.EVT_MENU, self.onClose, id=13)
+        
         self.SetIcon(wx.Icon('icons/shape_square.png', wx.BITMAP_TYPE_PNG))
         
         self.Show()
+        
+    def doMonitor(self, event):
+        print '#Start Monitor'
+        
+        if self.smtp_mgnr==None or not self.smtp_mgnr.thread.isAlive():
+            print 'Server Not started to Monitor' 
+            return
+            
+        if self.mon_thread!=None and self.mon_thread.isAlive():
+            print 'Already Monitoring'
+            return
+                
+        self.mon_thread =  threading.Thread(target=self.checkForNewEmails )
+        self.mon_thread.start()
+    
+    def stopMonitor(self, event):
+        print '#Stop Monitor'
+        if self.mon_thread==None or not self.mon_thread.isAlive():
+            print 'Not monitoring'
+            return
+        
+        self.stop_mon = True    
+        
+    def checkForNewEmails(self):
+        
+        while not self.stop_mon:
+
+            if smtp_ss.last_eml=="":
+                continue
+            
+            if self.last_eml != smtp_ss.last_eml:
+                print 'You Have New Email'+  smtp_ss.last_eml
+                self.main_panel.ET_panel.loadEmailFiles(None)
+                self.last_eml = smtp_ss.last_eml
+
+            #time.sleep(1)
+        
+        print 'Stopped Monitoring for Mails'
  
     def startSMTPSS(self, event):
         if self.smtp_mgnr!=None and self.smtp_mgnr.thread.isAlive():
-            print 'Already Started'
+            print 'Already Running'
             return
         
         self.smtp_mgnr = smtp_ss.ServerManager()
@@ -138,8 +223,8 @@ class MainFrame(wx.Frame):
             print 'Not Running'
             return
             
+        self.stopMonitor(event)    
         self.smtp_mgnr.stop()
-        print 'After Stop'
                 
     def onClose(self, event):
         self.stopSMTPSS(event)
--- smtp_simulator_1/simulator/smtp_simulator_server.py
+++ smtp_simulator_1/simulator/smtp_simulator_server.py
@@ -4,14 +4,48 @@ Created on Nov 27, 2014
 @author: Sarraju
 '''
 
-import smtpd, threading, asyncore, time, socket
+import smtpd, threading, asyncore, socket, os
+from datetime import datetime
+
+last_eml = ""
 
 class SMTPSimulationServer(smtpd.SMTPServer):
     def process_message(self, peer, mailfrom, rcpttos, data):
-        f = open(time.strftime('%d-%m-%Y_%H%M%S')+'.eml','a+b')
-        print data
-        f.write(data)
-        f.close()
+        self.thread =  threading.Thread(self.storeEmail(data))
+        self.thread.start()
+        
+    def storeEmail(self, data):
+        #print 'Email: '+data
+        try:
+            file_name = self.getFileName()+'.eml'
+            f = open('emails/'+file_name,'a+b')
+            f.write(data)
+            f.close()
+            global last_eml
+            last_eml = file_name
+        except Exception as e:
+            print 'log it'+e
+        
+    def getFileName(self):
+        
+        fileid = 'FILENAME_GENERATION_ERROR'
+        
+        try:
+            fileid = datetime.now().strftime('%d%m%Y%H%M%S%f')
+            
+            c = 0
+            while os.path.exists('emails/'+fileid+'.eml') and c != 100:
+                fileid = str(int(fileid) + 1)
+                c += 1
+            
+            if c==100:
+                return 'FILENAME_GENERATION_ERROR'
+       
+        except Exception as e:
+            print e
+            return 'FILENAME_GENERATION_ERROR'
+
+        return fileid
         
 
 class ServerManager(object):
@@ -19,14 +53,15 @@ class ServerManager(object):
     smtp = None
     
     def start(self):
+        '''
         if not self.canStartServer():
             print 'Cannot start Server.'
             #return
-        
+        '''
         self.smtp = SMTPSimulationServer(('', 25), None)
         self.thread =  threading.Thread(target=asyncore.loop,kwargs = {'timeout':1} )
         self.thread.start()
-        print 'Done'     
+        print 'Started'     
 
     def stop(self):
         if self.smtp!=None and self.smtp.accepting:
@@ -38,6 +73,7 @@ class ServerManager(object):
         return self.isPortOpen()
     
     def isPortOpen(self):
+        result = None
         try:
             s = socket.socket()
             result = s.connect(('', 25))
